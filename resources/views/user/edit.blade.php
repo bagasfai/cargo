11a5e4cda9ecde128c8e99d1c9f47e35ad8f044f
Muhammad Bagaskara <bagasfai50@gmail.com>
2026-04-13 14:30:22 +0700

    feat: Update user edit form and routing for consistency

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Tag Blog')
+@section('title', 'Edit User')
 
 @section('content')
     <div class="max-w-5xl mx-auto space-y-6">
 
         {{-- Header --}}
         <div class="flex justify-between items-center">
-            <x-ui.heading level="h1">Edit Category Blog</x-ui.heading>
+            <x-ui.heading level="h1">Edit User</x-ui.heading>
 
-            <a href="{{ route('blog-categories.index') }}" class="text-sm text-gray-600 dark:text-gray-400 hover:underline">
-                ← Back to Blog Categories
+            <a href="{{ route('users.index') }}" class="text-sm text-gray-600 dark:text-gray-400 hover:underline">
+                ← Back to Users
             </a>
         </div>
 
-        <form action="{{ route('blog-categories.update', $blogCategory) }}" method="POST" class="space-y-6">
+        <form action="{{ route('users.update', $user) }}" method="POST" class="space-y-6">
             @csrf
             @method('PUT')
             <div class="grid grid-cols-1 gap-6">
                 <div class="lg:col-span-2 space-y-6">
-                    <x-form.input name="name" label="Name" placeholder="Enter category name" :value="old('name', $blogCategory->name)" />
+                    <x-form.input name="name" label="Name" placeholder="Enter user name" :value="old('name', $user->name)" />
                     <x-form.textarea name="description" label="Description"
-                        placeholder="Short description for SEO & preview" :value="old('description', $blogCategory->description)" />
+                        placeholder="Short description for SEO & preview" :value="old('description', $user->description)" />
                 </div>
             </div>
 
             {{-- Actions --}}
             <div class="flex justify-end gap-3 pt-4">
-                <a href="{{ route('blog-categories.index') }}"
+                <a href="{{ route('users.index') }}"
                     class="px-4 py-2 rounded-lg border
                       text-gray-700 dark:text-gray-300
                       border-gray-300 dark:border-gray-700">
